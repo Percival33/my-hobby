40107c7fe8c94bc20b99ede644a8909055db2a9b
Marcin Jarczewski <marcin.jarc@gmail.com>
2021-11-05 22:12:03 +0100

    main.js working on console

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#submit').onclick = () => {
         let sail = document.querySelectorAll('input[name="checkbox"]:checked');
-        console.log(sail.length);
+        let color = document.querySelector('#color').value;
+        let radio = document.querySelector('input[name="radio"]:checked').value; 
+        let len = document.querySelector('#range').value;
+        let boat = document.querySelector('#text').value;
         for(let i = 0; i < sail.length; i++) {
             console.log(sail[i].value);
         }
-        // let color = document.querySelector('#color').value;
-        // let radio = document.querySelector('input[name="radio"]:checked').value; 
-        // let len = document.querySelector('#range').value;
-        // let boat = document.querySelector('#text').value;
-        // console.log(color);
-        // console.log(radio);
-        // console.log(len);
-        // // console.log(sail);
-        // console.log(boat);
+        console.log(color);
+        console.log(radio);
+        console.log(len);
+        console.log(boat);
 
         return false;
     };  
