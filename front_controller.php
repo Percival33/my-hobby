b2f4e817b884afadc2f7713703aa2520af1366b9
Marcin Jarczewski <marcin.jarc@gmail.com>
2021-12-22 14:50:59 +0100

    moved files outside DocumentRoot

--- front_controller.php
+++ front_controller.php
@@ -1,12 +1,21 @@
 <?php
-require_once 'controllers.php';
+require_once '../controllers.php';
 
 // obsługa  sesji?
 // require access.php ?
 
-require_once 'routing.php';
+require_once '../routing.php';
 
 $action_url = $_GET['action'];
-
 $controller_name = $routing[$action_url];
 $controller_name();
+
+// if(in_array($action_url, $routing)) {
+//   echo "Jest";
+//   exit();
+// }
+// else {
+//   $controller_name = $routing['/error'];
+//   $controller_name();
+//   exit();
+// }
